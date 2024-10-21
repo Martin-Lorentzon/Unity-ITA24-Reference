2adf025b1b52e22c071c16999c3bc00c20f7f080
Martin Lorentzon <106841201+Martin-Lorentzon@users.noreply.github.com>
2024-10-21 16:33:15 +0200

    Pivot camera start

---
+++ Assets/Scripts/Pivot Camera/PivotLook.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PivotLook : MonoBehaviour
+{
+    public float sensX = 500f;
+    public float sensY = 500f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
